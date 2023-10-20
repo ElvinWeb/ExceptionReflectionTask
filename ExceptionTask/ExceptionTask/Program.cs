b2d56e762825a5062d008be40b4fa44281593afb
Elvin <elvinsrkrov@gmail.com>
2023-10-20 10:28:41 +0400

    CalculateSalary method added

--- ExceptionTask/ExceptionTask/Program.cs
+++ ExceptionTask/ExceptionTask/Program.cs
@@ -5,17 +5,13 @@ namespace ExceptionTask
 {
     internal class Program
     {
+        static Employee[] employees = Array.Empty<Employee>();  
         static void Main(string[] args)
         {
             #region Try-Catch bloks
-
-
-
+             
             try
             {
-                Employee empolyee1 = new Employee("Samir Agayev", "HR", 19, 5, 1200);
-                Employee empolyee2 = new Employee("Elvin Sarkarov", "IT", 23, 8, 1700);
-                Employee empolyee3 = new Employee("Amil Amilov", "HR", 25, 10, 2000);
 
             }
             catch (InvalidFullNameException ex)
@@ -41,21 +37,40 @@ namespace ExceptionTask
 
             #endregion
 
-          
-            CalculateSalary();
+            Employee empolyee1 = new Employee("Samir Agayev", "HR", 19, 5, 1200);
+            Employee empolyee2 = new Employee("Elvin Sarkarov", "IT", 23, 8, 1700);
+            Employee empolyee3 = new Employee("Amil Amilov", "HR", 25, 10, 2000);
+            AddEmployee(empolyee1);
+            AddEmployee(empolyee2);
+            AddEmployee(empolyee3);
+
+
+
+            Console.WriteLine($"Sum of the employee salary:{CalculateSalary()}");
 
         }
 
-        static void CalculateSalary()
+        static void AddEmployee(Employee employee)
+        {
+            Array.Resize(ref employees, employees.Length + 1);
+            employees[employees.Length - 1] = employee;
+        }
+
+        static int CalculateSalary()
         {
             Type employeeType = typeof(Employee);
-            double sum = 0;
-            BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
-            var myFieldInfo = employeeType.GetField("_salary", flags);
-            
-            
-            
+            int sum = 0;
 
+            BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var salaryInfo = employeeType.GetField("_salary", flags);
+
+            foreach(Employee employee in employees)
+            {
+                
+                sum += Convert.ToInt32(salaryInfo.GetValue(employee));
+            }
+
+            return sum;
             
             
         }
